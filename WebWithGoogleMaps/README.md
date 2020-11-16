0db3f7cf8ef100d6a75b9629d2ee37b1f754f24a
filip.piwowarczyk <filip.piwowarczyk@palturai.com>
2020-11-16 14:55:42 +0100

    Add module 25 and GMaps proj

---
+++ WebWithGoogleMaps/README.md
@@ -0,0 +1,3 @@
+# Website with Google Maps API
+
+It is web site that use google maps api to locate you. 
---
+++ WebWithGoogleMaps/src/UI/Modal.js
@@ -0,0 +1,39 @@
+export class Modal {
+
+    constructor(contentId, fallbackText) {
+        this.fallbackText = fallbackText
+        this.contentTemplateEl = document.getElementById(contentId);
+        this.modalTemplateEl = document.getElementById('modal-template');
+
+    }
+
+    show() {
+        if (this.checkSupportForTemplateTag()) {
+            const modalElements = document.importNode(this.modalTemplateEl.content, true);
+            this.modalElement = modalElements.querySelector('.modal')
+            this.backdropElement = modalElements.querySelector('.backdrop')
+            const contentElement = document.importNode(this.contentTemplateEl.content, true)
+
+            this.modalElement.appendChild(contentElement);
+
+            document.body.insertAdjacentElement('afterbegin', this.modalElement);
+            document.body.insertAdjacentElement('afterbegin', this.backdropElement);
+        } else {
+            //fallback code
+            alert(this.fallbackText);
+        }
+    }
+
+    hide() {
+        if (this.modalElement) {
+            document.body.removeChild(this.backdropElement);
+            document.body.removeChild(this.modalElement);
+            this.modalElement = null;
+            this.backdropElement = null;
+        }
+    }
+
+    checkSupportForTemplateTag() {
+        return 'content' in document.createElement('template');
+    }
+}
